ce6319d0d4efed6d2b27ecee4c27b5421e880ed9
Jibran <jibraniqbal@protonmail.com>
2019-04-18 22:01:56 +0500

    added server config

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 // Set up the express app
 const app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/api/cookie', (req, res) => {
   res.status(200).send({
     success: 'true',
@@ -9,8 +11,6 @@ app.get('/api/cookie', (req, res) => {
   })
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`server running on port ${PORT}`)
 });
